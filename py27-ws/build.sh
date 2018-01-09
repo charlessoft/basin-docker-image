40dbbd04189818f1125fa136710b4d719c37a116
charlessoft <charlessoft@qq.com>
2018-01-09 11:15:19 +0800

    py27-ws image

--- py27-ws/build.sh
+++ py27-ws/build.sh
@@ -1,1 +1,24 @@
-docker build -t basin:py27-ws .
+#!/bin/bash
+image_name=basin:py27-ws
+version=latest
+#docker build -t -python:2.7 .
+
+containerlist=$(docker ps -a -q)
+for container in ${containerlist[@]}
+do
+    docker rm -f $container
+done
+
+imagelist=$(docker images -q)
+for image in ${imagelist[@]}
+do
+    docker rmi -f $image
+done
+
+
+#make build_all
+
+docker build -t $image_name:$version .
+docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
+docker push 127.0.0.1:5006/$image_name:$version
+
