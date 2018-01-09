c3eb5b7cfb377d0a1798a4579e3cdcac52009355
charlessoft <charlessoft@qq.com>
2018-01-09 17:02:43 +0800

    build

--- py27-ml/build.sh
+++ py27-ml/build.sh
@@ -3,11 +3,20 @@ image_name=basin/py27-ml
 version=latest
 #docker build -t -python:2.7 .
 
+function build(){
+	if [ ! -n "$1" ] ;then
+		version=latest
+	else
+		version=$1
+	fi
+	echo 'build version:' $version
+	docker build -t $image_name:$version .
+	docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
+	docker push 127.0.0.1:5006/$image_name:$version
 
+
+}
 
 #make build_all
 
-docker build -t $image_name:$version .
-docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
-docker push 127.0.0.1:5006/$image_name:$version
-
+build $*
--- py27-spider/build.sh
+++ py27-spider/build.sh
@@ -3,11 +3,21 @@ image_name=basin/py27-spider
 version=latest
 #docker build -t -python:2.7 .
 
+function build(){
+	if [ ! -n "$1" ] ;then
+		version=latest
+	else
+		version=$1
+	fi
+	echo 'build version:' $version
+	docker build -t $image_name:$version .
+	docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
+	docker push 127.0.0.1:5006/$image_name:$version
 
+
+}
+
+build $*
 
 #make build_all
-
-docker build -t $image_name:$version .
-docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
-docker push 127.0.0.1:5006/$image_name:$version
 
