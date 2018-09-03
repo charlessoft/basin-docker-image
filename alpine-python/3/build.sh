d30094946fb68e8aae5af02dcd9d4d1666a505f3
charlessoft <charlessoft@qq.com>
2018-09-04 00:49:02 +0800

    py3

---
+++ alpine-python/3/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+image_name=basin/alpine-python3
+version=latest
+#docker build -t -python:2.7 .
+
+function build(){
+	if [ ! -n "$1" ] ;then
+		version=latest
+	else
+		version=$1
+	fi
+	docker build -t $image_name:$version .
+	#docker tag $image_name:$version 127.0.0.1:5006/$image_name:$version
+	#docker push 127.0.0.1:5006/$image_name:$version
+
+}
+
+build $*
